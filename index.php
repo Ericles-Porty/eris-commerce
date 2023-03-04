810f41b4837d0aa7cb2946cc9d3a3ef590803089
ericles-porty <ericlesdsantos@gmail.com>
2023-03-04 02:21:32 -0300

    [Add] Add admin auth

--- index.php
+++ index.php
@@ -1,14 +1,20 @@
 <?php
 
-use \Slim\Slim;
+session_start();
+require_once("vendor/autoload.php");
+
+use \Slim\App;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Models\User;
 
 require_once("vendor/autoload.php");
 
-$app = new Slim();
+$config = ['settings' => [
+	'addContentLengthHeader' => false,
+]];
 
-$app->config('debug', true);
+$app = new App($config);
 
 $app->get('/', function () {
 
@@ -18,9 +24,38 @@ $app->get('/', function () {
 });
 
 $app->get('/admin', function () {
+
+	User::verifyLogin();
+
 	$page = new PageAdmin();
 
 	$page->setTpl("index");
+});
+
+$app->get('/admin/login', function () {
+
+	$page = new PageAdmin([
+		"header" => false,
+		"footer" => false
+	]);
+
+	$page->setTpl("login");
+});
+
+$app->post('/admin/login', function () {
+	User::login($_POST['login'], $_POST['password']);
+
+	header("Location: /admin");
+	exit;
+});
+
+
+$app->get('/admin/logout', function () {
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
 });
 
 $app->run();
